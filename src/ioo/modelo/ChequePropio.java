61790434d31f69633d290c8ed5c97bb1b37b95d8
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 13:52:36 -0300

    Agrega cheque propio

---
+++ src/ioo/modelo/ChequePropio.java
@@ -0,0 +1,10 @@
+package ioo.modelo;
+
+public class ChequePropio extends Cheque {
+
+	public ChequePropio(int numero, String tipo, double monto) {
+		
+		super(numero, tipo, monto);
+		// TODO Auto-generated constructor stub
+	}
+}
