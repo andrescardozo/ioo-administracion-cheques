12923d202c1f506e2284b9b43206760bf5d43609
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 20:41:10 -0300

    Agrega el comportamiento y la composicion con cheque

--- src/uade/ioo/modelo/AdministradorDePagos.java
+++ src/uade/ioo/modelo/AdministradorDePagos.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdministradorDePagos {
+	
+	private Cheque cheque;
 
-	public List<Cheque> calcularChequesParaPagar(double monto) {
-		
-		Cheque uno = new Cheque(12323, "Terceros", monto / 2); 
-		Cheque dos = new Cheque(1, "Propio", monto / 2); 
-		
+	public Cheque getCheque() {
+		return cheque;
+	}
+
+	public List<Cheque> obtenerChequesParaPagar(double monto) {
+
+		Cheque uno = new Cheque(12323, "Terceros", monto / 2);
+		Cheque dos = new Cheque(1, "Propio", monto / 2);
+
 		List<Cheque> cheques = new ArrayList<Cheque>();
 		cheques.add(uno);
 		cheques.add(dos);
-		
+
 		return cheques;
+	}
+
+	public Cheque generarChequeNuevo(double monto) {
+
+		return new Cheque(2, "Propio", monto);
+	}
+
+	public double getMontoDiponiblePagos() {
+
+		return 1;
 	}
 }
