97a1ad9199f43fc9c2f3ba6d5586afcef12170a1
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 17:46:58 -0300

    Agrega el formulario principal

---
+++ src/uade/ioo/vista/formularios/JFormularioPrincipal.java
@@ -0,0 +1,16 @@
+package uade.ioo.vista.formularios;
+
+import javax.swing.JFrame;
+
+public class JFormularioPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public JFormularioPrincipal() {
+		
+		this.setSize(640, 480);
+		this.setTitle("Administrador de cheques");
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
