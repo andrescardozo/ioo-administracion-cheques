f663a0531454cbbd036479aedaa3dc0fdbfcc286
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 17:46:40 -0300

    Agrega el programa principal

---
+++ src/uade/ioo/principal/Programa.java
@@ -0,0 +1,14 @@
+package uade.ioo.principal;
+
+import javax.swing.JFrame;
+
+import uade.ioo.vista.formularios.JFormularioPrincipal;
+
+public class Programa {
+	
+	public static void main (String[] args) {
+		
+		JFrame formularioPrincipal = new JFormularioPrincipal();
+		formularioPrincipal.setVisible(true);
+	}
+}
