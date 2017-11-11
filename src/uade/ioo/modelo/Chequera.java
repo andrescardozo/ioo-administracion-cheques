a56dbb2251ba4dc6604df49ed259ab4fc4c24236
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 20:41:37 -0300

    Incrementa el numero de cheque

--- src/uade/ioo/modelo/Chequera.java
+++ src/uade/ioo/modelo/Chequera.java
@@ -1,9 +1,13 @@
 package uade.ioo.modelo;
 
 public class Chequera {
+	
+	private static int ULTIMO_NUMERO_FACTURA = 0;
 
 	public double ultimoNumero() {
 		
-		return 1;
+		Chequera.ULTIMO_NUMERO_FACTURA = Chequera.ULTIMO_NUMERO_FACTURA + 1;
+		
+		return ULTIMO_NUMERO_FACTURA;
 	}
 }
