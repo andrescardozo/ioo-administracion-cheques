25f5d640ae0f236c681e10823bb95c2f2c298a21
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 20:45:08 -0300

    Agrega la composicion con cheque propio

--- src/uade/ioo/modelo/Chequera.java
+++ src/uade/ioo/modelo/Chequera.java
@@ -1,13 +1,19 @@
 package uade.ioo.modelo;
 
 public class Chequera {
-	
+
 	private static int ULTIMO_NUMERO_FACTURA = 0;
 
+	private ChequePropio chequePropio;
+
+	public ChequePropio getChequePropio() {
+		return chequePropio;
+	}
+
 	public double ultimoNumero() {
-		
+
 		Chequera.ULTIMO_NUMERO_FACTURA = Chequera.ULTIMO_NUMERO_FACTURA + 1;
-		
+
 		return ULTIMO_NUMERO_FACTURA;
 	}
 }
