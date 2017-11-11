1e6fb85673634ce4642e0ce6a29b5d6323ce4973
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-11 20:51:46 -0300

    Agrega la composicion con estado de cheque

--- src/uade/ioo/modelo/ChequeTerceros.java
+++ src/uade/ioo/modelo/ChequeTerceros.java
@@ -1,11 +1,16 @@
 package uade.ioo.modelo;
 
 public class ChequeTerceros extends Cheque {
+	
+	private EstadoCheque estadoCheque;
 
+	public EstadoCheque getEstadoCheque() {
+		return estadoCheque;
+	}
+	
 	public ChequeTerceros(int numero, String tipo, double monto) {
 		
 		super(numero, tipo, monto);
 		// TODO Auto-generated constructor stub
 	}
-
 }
