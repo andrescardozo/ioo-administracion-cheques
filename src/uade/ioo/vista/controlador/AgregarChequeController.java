d0265715b948247d07b16317cd3faf41d0102029
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-25 23:24:04 -0300

    Agrega AgregarChequeController

---
+++ src/uade/ioo/vista/controlador/AgregarChequeController.java
@@ -0,0 +1,30 @@
+package uade.ioo.vista.controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import uade.ioo.modelo.AdministradorDePagos;
+import uade.ioo.modelo.ChequeTerceros;
+import uade.ioo.vista.comportamiento.IVistaRecibirCheque;
+
+public class AgregarChequeController implements ActionListener {
+
+	private AdministradorDePagos modelo;
+	private IVistaRecibirCheque vista;
+
+	public AgregarChequeController(AdministradorDePagos modelo, IVistaRecibirCheque vista) {
+		
+		super();
+		this.modelo = modelo;
+		this.vista = vista;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		ChequeTerceros chequeTerceros = new ChequeTerceros(this.vista.getNumero(), this.vista.getMonto());
+
+		this.modelo.registrarChequeTercero(chequeTerceros);
+	}
+
+}
