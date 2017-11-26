c691d908d19eaccfc0ad0b84dfecfd0ce1f830d4
Andres Cardozo <acardozoreimer@gmail.com>
2017-11-25 23:33:07 -0300

    Implementa el AdministradorDePagos

--- src/uade/ioo/principal/Programa.java
+++ src/uade/ioo/principal/Programa.java
@@ -2,13 +2,16 @@ package uade.ioo.principal;
 
 import javax.swing.JFrame;
 
+import uade.ioo.modelo.AdministradorDePagos;
 import uade.ioo.vista.formularios.JFormularioReporte;
 
 public class Programa {
 	
 	public static void main (String[] args) {
 		
-		JFrame formularioPrincipal = new JFormularioReporte();
+		AdministradorDePagos modelo = new AdministradorDePagos();
+		
+		JFrame formularioPrincipal = new JFormularioReporte(modelo);
 		formularioPrincipal.setVisible(true);
 	}
 }
